a56bcf9c452a1d5ac45abf611bc45a90996d4b9a
Jonathan RUAS <jonathan.ruas@redfabriq.com>
2018-11-09 19:01:33 +0100

    Force SSL

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,8 +15,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // Fix key length
         Schema::defaultStringLength(191);
-        //
+        // Force SSL in production
+        if ($this->app->environment() == 'production') {
+            URL::forceScheme('https');
+        }
     }
 
     /**
