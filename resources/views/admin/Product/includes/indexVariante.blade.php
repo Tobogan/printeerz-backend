efefcc64e22a9f8b206bce134d9c62ab06053193
Jonathan RUAS <jonathan.ruas@redfabriq.com>
2019-02-03 16:22:28 +0100

    Product show view (not finish)

--- resources/views/admin/Product/includes/indexVariante.blade.php
+++ resources/views/admin/Product/includes/indexVariante.blade.php
@@ -18,10 +18,11 @@
             </div>
         </div>
     </div>
-
+    <?php $i = 0 ?>
     @foreach ($products_variants as $products_variant)
-    @if($products_variant->product_id == $product->id)
+        @if($products_variant->product_id == $product->id)
         {{-- if have results --}}
+        <?php $i++ ?>
         <div class="card-header">
             <div class="row align-items-center">
                 <div class="col">
@@ -62,32 +63,32 @@
                 </thead>
                 <tbody class="list">
                     @foreach ($products_variants as $products_variant)
-                        @if($products_variant->product_id == $product->id)
-                            <tr>
-                                <td class="products_variant-color">{{ $products_variant->color }}</td>
-                                <td class="products_variant-size">{{ $products_variant->size }}</td>
-                                <td class="products_variant-quantity">{{ $products_variant->quantity }}</td>
-                                <td class="text-right">
-                                    <div class="dropdown">
-                                        <a href="!" class="dropdown-ellipses dropdown-toggle" role="button" data-toggle="dropdown"
-                                            aria-haspopup="true" aria-expanded="false" data-boundary="window">
-                                            <i class="fe fe-more-vertical"></i>
-                                        </a>
-                                        <div class="dropdown-menu dropdown-menu-right">
-                                            @if ($products_variant->is_active == true)
-                                            <a class="dropdown-item" onclick="return confirm('Êtes-vous sûr?')" href="{{route('desactivate_productsVariants', $products_variant->id)}}">
-                                                Désactiver </a>
-                                            @else
-                                            <a class="dropdown-item" href="{{route('activate_productsVariants', $products_variant->id)}}">Activer</a>
-                                            @endif
-                                            <hr class="dropdown-divider">
-                                            <a class="dropdown-item text-danger" onclick="return confirm('Êtes-vous sûr?')" href="{{route('destroy_productsVariants', $products_variant->id)}}">
-                                                Supprimer </a>
-                                        </div>
-                                    </div>
-                                </td>
-                            </tr>
-                        @endif
+                    @if($products_variant->product_id == $product->id)
+                    <tr>
+                        <td class="products_variant-color">{{ $products_variant->color }}</td>
+                        <td class="products_variant-size">{{ $products_variant->size }}</td>
+                        <td class="products_variant-quantity">{{ $products_variant->quantity }}</td>
+                        <td class="text-right">
+                            <div class="dropdown">
+                                <a href="!" class="dropdown-ellipses dropdown-toggle" role="button" data-toggle="dropdown"
+                                    aria-haspopup="true" aria-expanded="false" data-boundary="window">
+                                    <i class="fe fe-more-vertical"></i>
+                                </a>
+                                <div class="dropdown-menu dropdown-menu-right">
+                                    @if ($products_variant->is_active == true)
+                                    <a class="dropdown-item" onclick="return confirm('Êtes-vous sûr?')" href="{{route('desactivate_productsVariants', $products_variant->id)}}">
+                                        Désactiver </a>
+                                    @else
+                                    <a class="dropdown-item" href="{{route('activate_productsVariants', $products_variant->id)}}">Activer</a>
+                                    @endif
+                                    <hr class="dropdown-divider">
+                                    <a class="dropdown-item text-danger" onclick="return confirm('Êtes-vous sûr?')" href="{{route('destroy_productsVariants', $products_variant->id)}}">
+                                        Supprimer </a>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                    @endif
                     @endforeach
                 </tbody>
             </table>
@@ -96,27 +97,22 @@
             <ul class="pagination">
             </ul>
         </div>
-    @endif
+        @endif
     @endforeach
-
-    {{-- If no product variant --}}
-    {{-- @foreach ($products_variants as $products_variant)
-        @if(!$products_variant->product_id == $product->id)
-            <div class="card-body text-center">
-                <img src="/img/svg/team_spirit.svg" alt="..." class="img-fluid" style="max-width: 182px;">
-                <h1>
-                    Pas de variantes.
-                </h1>
-                <p class="text-muted">
-                    Ajouter la première variante du produit
-                </p>
-                <a href="#" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#addVariante">
-                    Ajouter une variante
-                </a>
-            </div>
-            @break
-        @endif 
-    @endforeach --}}
+    @if($i == 0)
+    <div class="card-body text-center">
+        <img src="/img/svg/team_spirit.svg" alt="..." class="img-fluid" style="max-width: 182px;">
+        <h1>
+            Pas de variantes.
+        </h1>
+        <p class="text-muted">
+            Ajouter la première variante du produit
+        </p>
+        <a href="#" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#addVariante">
+            Ajouter une variante
+        </a>
+    </div>
+    @endif
 </div>
 
 @include('admin.Product.includes.addVariante')
