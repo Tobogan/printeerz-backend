0fc6b1de33f60ee5eef51e889e681619f3d22edb
Jonathan RUAS <jonathan.ruas@redfabriq.com>
2019-03-09 21:59:06 +0100

    Add new Alerts style Fix Upload file

--- resources/views/admin/Customer/add.blade.php
+++ resources/views/admin/Customer/add.blade.php
@@ -24,213 +24,180 @@
                     </div>
                 </div>
             </div>
+
             {{-- Body --}}
-            @if ($errors->any())
-            <div class="alert alert-danger" role="alert">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-            @endif
             {!! Form::open(['action' => array('CustomerController@store'), 'files' => true,
             'class' => 'mb-4']) !!}
-            <div class="row">
-                {{csrf_field()}}
-                <div class="col-12">
-                    <!-- First name -->
-                    <div class="form-group">
-                        <!-- Label -->
-                        <label>
-                            Nom du client
-                        </label>
-                        <!-- Input -->
-                        {!! Form::text('title', null, ['class' => 'form-control', 'placeholder' => 'Nom du client'])
-                        !!}
-                    </div>
-                </div>
-                <div class="col-12 col-md-6">
-                    <!-- First name -->
-                    <div class="form-group">
-                        <!-- Label -->
-                        <label>
-                            Activité
-                        </label>
-                        <!-- Input -->
-                        {!! Form::text('activity_type', null, ['class' => 'form-control', 'placeholder' =>
-                        'Activité:']) !!}
-                    </div>
-                </div>
-                <div class="col-12 col-md-6">
-                    <!-- First name -->
-                    <div class="form-group">
-                        <!-- Label -->
-                        <label>
-                            SIREN
-                        </label>
-                        <!-- Input -->
-                        {!! Form::text('SIREN', null, ['class' => 'form-control', 'placeholder' => 'SIREN/SIRET']) !!}
-                    </div>
-                </div>
-            </div>
-            <hr class="mt-4 mb-5">
-            <div class="row">
-                <div class="col-12">
-                    <p class="h3">Adresse</p>
-                    <p class="mb-4">Entrez les informations de la personne avec laquelle vous êtes en contact.</p>
-                </div>
-                <div class="col-12">
-                    <!-- Address  -->
-                    <div class="form-group">
-                        <!-- Label -->
-                        <label>
-                            Adresse
-                        </label>
-                        <!-- Input -->
-                        {!! Form::text('address', null, ['class' => 'form-control', 'placeholder' => 'Adresse'])
-                        !!}
-                    </div>
-                </div>
-                <div class="col-12">
-                    {!! Form::hidden('longitude', null) !!}
-                    {!! Form::hidden('lattitude', null) !!}
-                </div>
-                <div class="col-12 col-md-6">
-                    <!-- First name -->
-                    <div class="form-group">
-                        <!-- Label -->
-                        <label>
-                            Code postal
-                        </label>
-                        <!-- Input -->
-                        {!! Form::text('postal_code', null, ['class' => 'form-control', 'placeholder' => 'Ville']) !!}
-                    </div>
-                </div>
-                <div class="col-12 col-md-6">
-                    <!-- First name -->
-                    <div class="form-group">
-                        <!-- Label -->
-                        <label>
-                            Ville
-                        </label>
-                        <!-- Input -->
-                        {!! Form::text('city', null, ['class' => 'form-control', 'placeholder' => 'Ville :']) !!}
-                    </div>
-                </div>
-                <div class="col-12">
-                    <label>
-                        Pays
-                    </label>
-                    {!! Form::text('country', null, ['class' => 'form-control', 'placeholder' => 'Pays :']) !!}
-                </div>
-            </div>
-            <hr class="mt-4 mb-5">
-            <div class="row">
-                <div class="col-12">
-                    <p class="h3">Nom du contact</p>
-                    <p class="mb-4">Entrez les informations de la personne avec laquelle vous êtes en contact.</p>
-                </div>
-                <div class="col-12 col-md-6">
-                    <!-- First name -->
-                    <div class="form-group">
-                        <!-- Label -->
-                        <label>
-                            Nom
-                        </label>
-                        <!-- Input -->
-                        {!! Form::text('lastname', null, ['class' => 'form-control', 'placeholder' => 'Nom']) !!}
-                    </div>
-                </div>
-                <div class="col-12 col-md-6">
-                    <!-- First name -->
-                    <div class="form-group">
-                        <!-- Label -->
-                        <label>
-                            Prénom
-                        </label>
-                        <!-- Input -->
-                        {!! Form::text('firstname', null, ['class' => 'form-control', 'placeholder' => 'Prénom']) !!}
-                    </div>
-                </div>
-                <div class="col-12 col-md-6">
-                    <!-- First name -->
-                    <div class="form-group">
-                        <!-- Label -->
-                        <label>
-                            Email
-                        </label>
-                        <!-- Input -->
-                        {!! Form::email('email', null, ['class' => 'form-control', 'placeholder' => 'Email']) !!}
-                    </div>
-                </div>
-                <div class="col-12 col-md-6">
-                    <!-- First name -->
-                    <div class="form-group">
-                        <!-- Label -->
-                        <label>
-                            Téléphone
-                        </label>
-                        {!! Form::text('phone', null, ['class' => 'form-control', 'placeholder' => 'Téléphone',
-                        'data-mask' => '00 00 00 00 00']) !!}
-                    </div>
-                </div>
-                <div class="col-12 col-md-6">
-                    <!-- First name -->
-                    <div class="form-group">
-                        <!-- Label -->
-                        <label>
-                            Poste
-                        </label>
-                        {!! Form::text('job_title', null, ['class' => 'form-control', 'placeholder' => 'Poste'])!!}
-                    </div>
-                </div>
-            </div>
-            <hr class="mt-4 mb-5">
-            <div class="row">
-                <div class="col-12">
-                    <p class="h3">Logo</p>
-                    <p class="mb-4">Ajouter le logo du client en format 1:1</p>
-                </div>
-                <div class="col-12">
-                    <!-- First name -->
-                    <div class="form-group">
-                        <div class="dropzone dropzone-single mb-3" data-toggle="dropzone" data-dropzone-url="http://" id="logo_event_upload">
-                            <div class="fallback">
-                                <div class="custom-file">
-                                    {!! Form::file('image', array('class' => 'custom-file-input', 'id' => 'logo_img')) !!}
-                                    <label class="custom-file-label" for="projectCoverUploads">Choose file</label>
-                                </div>
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        {{csrf_field()}}
+                        <div class="col-12">
+                            <!-- First name -->
+                            <div class="form-group">
+                                <!-- Label -->
+                                <label>
+                                    Nom du client
+                                </label>
+                                <!-- Input -->
+                                {!! Form::text('title', null, ['class' => 'form-control' . $errors->first('title', ' is-invalid'), 'placeholder' => 'Nom du client']) !!}
+                                @if($errors->has('title'))<div class="invalid-feedback">Veuillez renseigner le nom du client</div>@endif
                             </div>
-                            <div class="dz-preview dz-preview-single">
-                                <div class="dz-preview-cover">
-                                    <img class="dz-preview-img" src="..." alt="..." data-dz-thumbnail>
+                        </div>
+                        <div class="col-12 col-md-6">
+                            <!-- First name -->
+                            <div class="form-group">
+                                <!-- Label -->
+                                <label>
+                                    Activité
+                                </label>
+                                <!-- Input -->
+                                {!! Form::text('activity_type', null, ['class' => 'form-control'. $errors->first('activity_type', ' is-invalid'), 'placeholder' =>'']) !!}
+                                @if($errors->has('activity_type'))<div class="invalid-feedback">Veuillez renseigner une activité</div>@endif
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-6">
+                            <!-- First name -->
+                            <div class="form-group">
+                                <!-- Label -->
+                                <label>
+                                    SIREN
+                                </label>
+                                <!-- Input -->
+                                {!! Form::text('SIREN', null, ['class' => 'form-control'. $errors->first('SIREN', ' is-invalid'), 'placeholder' => '012345678', 'data-mask' => '000000000' ]) !!}
+                                @if($errors->has('SIREN'))<div class="invalid-feedback">Veuillez renseigner un numéro de SIREN valide</div>@endif
+                            </div>
+                        </div>
+                    </div>
+                    <hr class="mt-4 mb-5">
+                    <div class="row">
+                        <div class="col-12">
+                            <p class="h3">Adresse</p>
+                            <p class="mb-4">Entrez les informations de la personne avec laquelle vous êtes en contact.</p>
+                        </div>
+                        <div class="col-12">
+                            <!-- Address  -->
+                            <div class="form-group">
+                                <!-- Label -->
+                                <label>
+                                    Adresse
+                                </label>
+                                <!-- Input -->
+                                <input class="form-control mt-2" id="formPlacesAuto" placeholder="Renseigner l'adresse" name="autocompleteAddress" type="text" autocomplete="false" onFocus="initMap();">
+                                <input class="form-control mt-2" name="address" id="address" type="hidden">
+                                <input class="form-control mt-2" name="postal_code" id="postal_code" type="hidden">
+                                <input class="form-control mt-2" name="city" id="city" type="hidden">
+                                <input class="form-control mt-2" name="country" id="country" type="hidden">
+                                <input class="form-control mt-2" name="lattitude" id="latitude" type="hidden">
+                                <input class="form-control mt-2" name="longitude" id="longitude" type="hidden">
+                            </div>
+                        </div>
+                    </div>
+                    <hr class="mt-4 mb-5">
+                    <div class="row">
+                        <div class="col-12">
+                            <p class="h3">Nom du contact</p>
+                            <p class="mb-4">Entrez les informations de la personne avec laquelle vous êtes en contact.</p>
+                        </div>
+                        <div class="col-12 col-md-6">
+                            <!-- First name -->
+                            <div class="form-group">
+                                <!-- Label -->
+                                <label>
+                                    Nom
+                                </label>
+                                <!-- Input -->
+                                {!! Form::text('lastname', null, ['class' => 'form-control', 'placeholder' => 'Nom'])
+                                !!}
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-6">
+                            <!-- First name -->
+                            <div class="form-group">
+                                <!-- Label -->
+                                <label>
+                                    Prénom
+                                </label>
+                                <!-- Input -->
+                                {!! Form::text('firstname', null, ['class' => 'form-control', 'placeholder' =>
+                                'Prénom']) !!}
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-6">
+                            <!-- First name -->
+                            <div class="form-group">
+                                <!-- Label -->
+                                <label>
+                                    Email
+                                </label>
+                                <!-- Input -->
+                                {!! Form::email('email', null, ['class' => 'form-control', 'placeholder' => 'Email']) !!}
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-6">
+                            <!-- First name -->
+                            <div class="form-group">
+                                <!-- Label -->
+                                <label>
+                                    Téléphone
+                                </label>
+                                {!! Form::text('phone', null, ['class' => 'form-control', 'placeholder' => 'Téléphone',
+                                'data-mask' => '00 00 00 00 00']) !!}
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-6">
+                            <!-- First name -->
+                            <div class="form-group">
+                                <!-- Label -->
+                                <label>
+                                    Poste
+                                </label>
+                                {!! Form::text('job_title', null, ['class' => 'form-control', 'placeholder' =>
+                                'Poste'])!!}
+                            </div>
+                        </div>
+                    </div>
+                    <hr class="mt-4 mb-5">
+                    <div class="row">
+                        <div class="col-12">
+                            <p class="h3">Logo</p>
+                            <p class="mb-4">Ajouter le logo du client en format 1:1</p>
+                        </div>
+                        <div class="col-12">
+                            <!-- First name -->
+                            <div class="form-group">
+                                <div class="custom-file">
+                                    {!! Form::file('image', array('class' => 'custom-file-input', 'id' => 'logo_img'))
+                                    !!}
+                                    <label class="custom-file-label" for="projectCoverUploads">Télécharger le logo</label>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
-            <hr class="mt-4 mb-5">
-            <div class="row">
-                <div class="col-12">
-                    <!-- First name -->
-                    <div class="form-group">
-                        <!-- Label -->
-                        <label>
-                            Evénements déjà organisés
-                        </label>
-                        <!-- Input -->
-                        {!! Form::select('shows_id[]', App\Event::pluck('name','_id'), null, ['class' => 'form-control', 'multiple', 'data-toggle' => 'select']) !!}
+                    <hr class="mt-4 mb-5">
+                    <div class="row">
+                        <div class="col-12">
+                            <!-- First name -->
+                            <div class="form-group">
+                                <!-- Label -->
+                                <label>
+                                    Evénements déjà organisés
+                                </label>
+                                <!-- Input -->
+                                {!! Form::select('shows_id[]', App\Event::pluck('name','_id'), null, ['class' =>
+                                'form-control', 'multiple', 'data-toggle' => 'select']) !!}
+                            </div>
+                        </div>
                     </div>
+                    {!! Form::hidden('is_active', "true") !!}
+                    {!! Form::hidden('is_deleted', "false") !!}
                 </div>
             </div>
-            {!! Form::hidden('is_active', "true") !!}
-            {!! Form::hidden('is_deleted', "false") !!}
             <div class="row">
                 <div class="col-12">
                     <div class="buttons">
-                        {!! Form::submit('Créer le client', ['class' => 'btn btn-primary', 'style' => 'float: right'])
+                        {!! Form::submit('Créer le client', ['class' => 'btn btn-primary', 'style' => 'float:
+                        right'])
                         !!}
                         <a class='btn btn-secondary' style="float: left" href="{{route('index_customer')}}">Annuler</a>
                     </div>
