55f22c9df0b5664d45b5422576e888b4d7593bba
Jonathan RUAS <jonathan.ruas@redfabriq.com>
2019-03-26 20:56:42 +0100

    Remove Event Toast timer

--- public/js/js.js
+++ public/js/js.js
@@ -31,8 +31,6 @@ $("#tabs").tabs({
   }
 });
 
-$('#element').toast('show');
-
 $('#addVariante').on('keypress', function (e) {
   if (e.keyCode === 13) {
     e.preventDefault();
--- resources/views/admin/Event/show.blade.php
+++ resources/views/admin/Event/show.blade.php
@@ -13,16 +13,4 @@
     @include('admin.Event.includes.addEventsProducts')
 </div>
 
-<div class="toast" role="alert" aria-live="assertive" aria-atomic="true" id="element" data-autohide="false">
-    <div class="toast-header">
-        <strong class="mr-auto">Début de l'évenement</strong>
-        <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-        </button>
-    </div>
-    <div class="toast-body">
-        Votre événement va débuter dans <b>2 jours !</b>
-    </div>
-</div>
-
 @endsection
