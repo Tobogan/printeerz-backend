1af26b2c5ae157dfb0308296e42a6e83ae496f0e
Jérémy Ruas <ruas.jeremy@gmail.com>
2019-05-04 10:57:17 +0200

    update du UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -92,4 +92,193 @@
             $exists = $disk->exists('file.jpg');
             return view('admin/User.index', ['users' => $users, 'disk' => $disk, 's3' => $s3, 'exists' => $exists]);
         }
+
+        /**
+         * Update the specified resource in storage.
+         *
+         * @param  \Illuminate\Http\Request  $request
+         * @param  int  $id
+         * @return \Illuminate\Http\Response
+         */
+        public function update(Request $request)
+        {
+            if (request('actual_email') == request('email')){
+                if($request->password != null) {
+                    $validatedData = $request->validate([
+                        'username' => 'required|string|max:255',
+                        'lastname' => 'required|string|max:255',
+                        'firstname' => 'required|string|max:255',
+                        'password' => 'string|min:6|confirmed',
+                        'profile_img' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048'
+                    ]);
+                }
+                else {
+                    $validatedData = $request->validate([
+                        'username' => 'required|string|max:255',
+                        'lastname' => 'required|string|max:255',
+                        'firstname' => 'required|string|max:255',
+                        'profile_img' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048'
+                    ]);
+                }
+                
+                $user_id = $request->user_id;
+                $user = User::find($user_id);
+
+                // Update Profile image
+                if ($request->hasFile('profile_img')){
+                    $disk = Storage::disk('s3');
+                    // Get current image path
+                    $oldPath = $user->profile_img;
+                    // Get new image
+                    $file = $request->file('profile_img');
+                    // Create image name
+                    $name = time() . $file->getClientOriginalName();
+                    // Define the new path to image
+                    $newFilePath = '/users/' . $name;
+                    // Resize new image
+                    $img = Image::make(file_get_contents($file))->heighten(80)->save($name);
+                    // Upload the new image
+                    $disk->put($newFilePath, $img, 'public');
+                    // Put in database
+                    $user->profile_img = $newFilePath;
+                    unlink(public_path() . '/' . $name);
+                    if(!empty($user->profile_img) && $disk->exists($newFilePath)){
+                        $disk->delete($oldPath);
+                    }
+                }
+
+                $user->lastname = $request->lastname;
+                $user->firstname = $request->firstname;
+                $user->username = $request->username;
+                $user->phone = $request->phone;
+                if($request->password) {
+                    $user->password = bcrypt($request->password);
+                }
+                $user->role = $request->role;
+                $user->phone = $request->phone;
+                $user->is_active = $request->is_active;
+                $user->is_deleted = $request->is_deleted; 
+            }
+            else {
+                if($request->password != null) {
+                    $validatedData = $request->validate([
+                        'username' => 'required|string|max:255',
+                        'lastname' => 'required|string|max:255',
+                        'firstname' => 'required|string|max:255',
+                        'password' => 'string|min:6|confirmed',
+                        'profile_img' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048'
+                    ]);
+                }
+                else {
+                    $validatedData = $request->validate([
+                        'username' => 'required|string|max:255',
+                        'lastname' => 'required|string|max:255',
+                        'firstname' => 'required|string|max:255',
+                        'profile_img' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048'
+                    ]);
+                }
+        
+                $user_id = $request->user_id;
+                $user = User::find($user_id);
+                $user->lastname = $request->lastname;
+                $user->firstname = $request->firstname;
+                $user->username = $request->username;
+                $user->phone = $request->phone;
+                $user->email = $request->email;
+                $user->phone = $request->phone;
+                $user->password = bcrypt($request->password);
+                $user->role = $request->role;
+                $user->is_active = $request->is_active;
+                $user->is_deleted = $request->is_deleted;
+                // Update Profile image
+                if ($request->hasFile('profile_img')){
+                    // Get current image path
+                    $oldPath = $user->profile_img;
+                    // Get new image
+                    $file = $request->file('profile_img');
+                    // Create image name
+                    $name = time() . $file->getClientOriginalName();
+                    // Define the new path to image
+                    $newFilePath = '/users/' . $name;
+                    // Resize new image
+                    $img = Image::make(file_get_contents($file))->heighten(80)->save($name);
+                    // Upload the new image
+                    $disk = Storage::disk('s3');
+                    $disk->put($newFilePath, $img, 'public-read');
+                    unlink(public_path() . '/' . $name);
+                    // Put in database
+                    $user->profile_img = $newFilePath;
+                    if(!empty($user->profile_img) && $disk->exists($newFilePath)){
+                        $disk->delete($oldPath);
+                    }
+                }
+            }
+            $user->save();
+            return redirect('admin/User/index');
+        }
+
+        /**
+         * Remove the specified resource from storage.
+         *
+         * @param  int  $id
+         * @return \Illuminate\Http\Response
+         */
+        public function destroy($id)
+        {
+            $user = User::find($id);
+            // Delete Profile image
+            $disk = Storage::disk('s3');
+            $filePath = $user->profile_img;
+            if(!empty($user->profile_img) && $disk->exists($filePath)){
+                $disk->delete($filePath);
+            }
+            $user->delete();
+            return redirect('admin/User/index');
+        }
+
+        public function desactivate($id)
+        {
+            $user = User::find($id);
+            $user->is_active = false;
+            $user->update();
+            return redirect('admin/User/index');
+        }
+
+        public function delete($id)
+        {
+            $user = User::find($id);
+            $user->is_deleted = true;
+            $user->update();
+            return redirect('admin/User/index');
+        }
+
+        public function activate($id)
+        {
+            $user = User::find($id);
+            $user->is_active = true;
+            $user->update();
+            return redirect('admin/User/index');
+        }
+
+        public function indexAPI()
+        {
+            $users = User::all();
+
+            return response()->json(
+                [
+                    'status' => 'success',
+                    'users' => $users->toArray()
+                ], 200);
+        }
+
+        public function showAPI(Request $request, $id)
+        {
+            $user = User::find($id);
+
+            return response()->json(
+                [
+                    'status' => 'success',
+                    'user' => $user->toArray()
+                ], 200);
+        }
     }
